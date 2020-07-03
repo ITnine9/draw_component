53bdfc769384d22397f222e275ece2247ce936f8
Jeffrey Wu <zhenzhenshow@163.com>
2020-07-03 09:22:51 +0800

    init

---
+++ code.js
@@ -0,0 +1,36 @@
+// const echarts = require('echarts');
+
+// var myChart = echarts.init(document.getElementById('main'));
+
+// // specify chart configuration item and data
+// var option = {
+//     title: {
+//         text: 'ECharts entry example'
+//     },
+//     tooltip: {},
+//     legend: {
+//         data: ['Sales']
+//     },
+//     xAxis: {
+//         data: ["shirt", "cardign", "chiffon shirt", "pants", "heels", "socks"]
+//     },
+//     yAxis: {},
+//     series: [{
+//         name: 'Sales',
+//         type: 'bar',
+//         data: [5, 20, 36, 10, 10, 20]
+//     }]
+// };
+
+// // use configuration item and data specified to show chart
+// myChart.setOption(option);
+
+$('.toggle').click(function() {
+    console.log('Clicked');
+    $('.slider').toggleClass('open');
+    if ( $(this).text() == '<<' ) {
+        $(this).text('>>');
+    } else {
+        $(this).text('<<');
+    }
+})
